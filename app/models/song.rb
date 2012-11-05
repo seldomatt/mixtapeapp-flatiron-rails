30ea9b73013911fcf9fd89fb5bab258052ba416b
Matthew Salerno <matthewsalern@gmail.com>
2012-11-05 17:23:41 -0500

    album song association

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,5 @@ class Song < ActiveRecord::Base
   attr_accessible :name
 
   belongs_to :artist
+  belongs_to :album
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121105220831) do
+ActiveRecord::Schema.define(:version => 20121105222155) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20121105220831) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "artist_id"
+    t.integer  "album_id"
   end
 
   create_table "users", :force => true do |t|
